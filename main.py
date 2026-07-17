c41bcb217ed6e4cf7b631d237b4902d2e71b085f
Mohit <ghsmohit2004@gmail.com>
2026-07-17 21:32:15 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+csv_path = os.path.join(BASE_DIR, "dataset", "labeled_dataset.csv")
+
+df = pd.read_csv(csv_path)
+print(df["fabric"])
